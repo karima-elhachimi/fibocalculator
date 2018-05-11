e4131e501296b4b68ab2548558927fac3223a001
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2018-05-11 16:01:24 +0200

    first commit

---
+++ math.js
@@ -0,0 +1,9 @@
+var fibonacci = exports.fibonacci = function(maxNumber) {
+    let sequence = [];
+    sequence[0] = 0;
+    sequence[1] = 0;
+    for (let i = 2; i <= maxNumber; i++) {
+        sequence[i] = sequence[i-2] + sequence[i-1];
+    }
+    return sequence;
+};
